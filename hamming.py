a8b55f59f2c77076ff22993de425228ee036b0b5
Ramona C Truta <ramona.truta@gmail.com>
2023-05-31 23:14:48 -0400

    Add hamming.py

---
+++ hamming.py
@@ -0,0 +1,13 @@
+def valid_strand(strand):
+    return len([letter for letter in strand if letter in "ACGT"]) == len(strand)
+
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Different lengths strands")
+    if not(valid_strand(strand_a)) or not(valid_strand(strand_b)):
+        return False    
+    dist = 0    
+    for index in range(0, len(strand_a)):
+        if strand_a[index] != strand_b[index]:
+            dist += 1 
+    return dist            
