15c5459289d0e4dd2ac2faf4aada66c696fdb812
Ramona C Truta <ramona.truta@gmail.com>
2023-06-01 19:59:25 -0400

    Add comments to armstrong_numbers.py

--- armstrong_numbers.py
+++ armstrong_numbers.py
@@ -1,11 +1,12 @@
+# Determines if a number is an Armstrong number.
 def is_armstrong_number(number):
-
+    """
+    An Armstrong number is a number that is the sum of its own digits,
+    each raised to the power of the number of digits.
+    """
     if number != int(number):
-
-        raise TypeError("Wrong input type")
-          
+        raise TypeError("Wrong input type")          
     length = len(str(number))
-
     return sum([int(digit)**length for digit in str(number)]) == number
 
         
