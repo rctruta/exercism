d18eae4e56144e7f5cbc2b2f6eca2deadb73b7e4
Ramona C Truta <ramona.truta@gmail.com>
2023-06-02 22:12:41 -0400

    Adding comments to isogram.py

--- isogram.py
+++ isogram.py
@@ -1,6 +1,10 @@
 from collections import defaultdict
-
+# Determine if a word or phrase is an isogram.
 def is_isogram(text) -> bool:
+    """
+    An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter, 
+    however spaces and hyphens are allowed to appear multiple times.
+    """
     if text == "":
         return True
     textList = [letter for letter in text.lower().strip() if letter >= 'a' and letter <= 'z']
