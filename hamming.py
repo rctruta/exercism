80c048cd1b18019ddf43d238c78d9e754825a1e7
Ramona C Truta <ramona.truta@gmail.com>
2023-06-02 22:17:34 -0400

    Adding comments to hamming.py

--- hamming.py
+++ hamming.py
@@ -1,7 +1,16 @@
 def valid_strand(strand):
+    """Validates the strand, making sure it contains only the ACGT letters."""
     return len([letter for letter in strand if letter in "ACGT"]) == len(strand)
 
 def distance(strand_a, strand_b):
+    """
+    Calculate the Hamming Distance between two DNA strands.
+    Example:
+    GAGCCTACTAACGGGAT
+    CATCGTAATGACGGCCT
+    ^ ^ ^  ^ ^    ^^    
+    They have 7 differences, and therefore, the Hamming Distance is 7.
+    """
     if len(strand_a) != len(strand_b):
         raise ValueError("Different lengths strands")
     if not(valid_strand(strand_a)) or not(valid_strand(strand_b)):
